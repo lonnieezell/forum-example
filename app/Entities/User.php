0bbcbc3b1643362a09aae566b34beb264468121d
Lonnie Ezell <lonnieje@gmail.com>
2023-08-27 22:22:09 -0500

    move discussion nav elements to app nav and add user menu stub.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -15,4 +15,68 @@ class User extends ShieldUser
     {
         return route_to('profile', $this->username);
     }
+
+    /**
+     * Renders out the user's avatar at the specified size (in pixels)
+     *
+     * @return string
+     */
+    public function renderAvatar(int $size = 52)
+    {
+        // Determine the color for the user based on their
+        // for existing users, email address since we know we'll always have that
+        // Use default hash if new user or the avatar is used as a placeholder
+
+        $idString = 'default-avatar-hash'; // Default avatar string
+
+        if ($this->id) {
+            if (setting('Users.avatarNameBasis') === 'name') {
+                $idString = $this->first_name ? $this->first_name[0] . ($this->last_name[0] ?? '') : $this->username[0] . $this->username[1];
+            } else {
+                $idString = $this->email[0] . $this->email[1];
+            }
+        }
+
+        $idString = strtoupper($idString);
+
+        $idValue = str_split($idString);
+        array_walk($idValue, static function (&$char) {
+            $char = ord($char);
+        });
+        $idValue = implode('', $idValue);
+
+        $colors = setting('Users.avatarPalette');
+
+        return view('users/_avatar', [
+            'user'       => $this,
+            'size'       => $size,
+            'fontSize'   => 20 * ($size / 52),
+            'idString'   => $idString,
+            'background' => $colors[$idValue % count($colors)],
+        ]);
+    }
+
+    /**
+     * Generates a link to the user Avatar
+     */
+    public function avatarLink(?int $size = null): string
+    {
+        if (isset($this->id) && empty($this->avatar)) {
+            // Default from Gravatar
+            if (setting('Users.useGravatar')) {
+                $hash = md5(strtolower(trim($this->email)));
+
+                return "https://www.gravatar.com/avatar/{$hash}?" . http_build_query([
+                    's' => ($size ?? 60),
+                    'd' => setting(
+                        'Users.gravatarDefault'
+                    ),
+                ]);
+            }
+        }
+
+        return ! empty($this->avatar)
+            ? base_url('/uploads/avatars/' . $this->avatar)
+            : '';
+    }
 }
--- themes/default/_app_nav.php
+++ themes/default/_app_nav.php
@@ -1,51 +1,79 @@
-<div class="navbar bg-base-300">
-    <div class="container flex mx-auto">
-        <div class="navbar-start flex-1">
+<div class="navbar bg-base-100" hx-boost="true">
+    <div class="container mx-auto flex justify-spread">
+        <!-- Mobile Menu -->
+        <div class="navbar-end flex-1">
             <div class="dropdown">
                 <label tabindex="0" class="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <!-- Mobile Menu -->
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1111</a></li>
-                    <li tabindex="0">
-                    <a class="justify-between">
-                        Parent
-                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                    </a>
-                    <ul class="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Item 1</a></li>
+                    <?php if(auth()->isLoggedIn()) : ?>
+                        <li>
+                            <a>Parents</a>
+                            <ul class="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                    <?php endif ?>
+                    <?php if(! auth()->isLoggedIn()) : ?>
+                        <li><a>Item 3</a></li>
+                    <?php endif ?>
                 </ul>
             </div>
-            <a href="<?= site_url() ?>" class="btn btn-ghost normal-case text-xl">
+
+            <!-- Site Name -->
+            <a class="btn btn-ghost normal-case text-xl">
                 <?= config('App')->siteName ?>
             </a>
         </div>
+
         <!-- Desktop Menu -->
-        <div class=" flex-0 navbar-center invisible md:visible text-end">
-            <ul class="menu menu-horizontal px-1">
-                <li><a href="<?= site_url('login') ?>">Sign In</a></li>
-            </ul>
-            <!-- <div class="flex-none gap-2">
-                <div class="dropdown dropdown-end">
-                    <label tabindex="0" class="btn">
-                        Account
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content">
-                        <li>
-                            <a class="justify-between">
-                                Profile
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1 gap-4 flex">
+                <li>
+                    <a href="<?= site_url('/categories') ?>"
+                        <?= url_is('categories') ? 'class="active"' : '' ?>>
+                        Categories
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= site_url('discussions') ?>"
+                        <?= url_is('discussions') ? 'class="active"' : '' ?>>
+                        Discussions
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= site_url('members') ?>"
+                        <?= url_is('members') ? 'class="active"' : '' ?>>
+                        Members
+                    </a>
+                </li>
+
+                <div class="form-control">
+                    <input type="text" placeholder="Search..." class="input input-bordered w-24 md:w-auto" />
                 </div>
-            </div> -->
+
+                <?php if(!auth()->loggedIn()) : ?>
+                    <li><a href="<?= route_to('register') ?>">Sign Up</a></li>
+                    <li><a href="<?= route_to('login') ?>">Sign In</a></li>
+                <?php endif ?>
+
+                <?php if(auth()->loggedIn()) : ?>
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                            <div class="w-10 rounded-full">
+                                <?= auth()->user()->renderAvatar() ?>
+                            </div>
+                        </label>
+                        <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <li><a class="w-full">Account</a></li>
+                            <li><a class="w-full" href="<?= route_to('logout') ?>">Logout</a></li>
+                        </ul>
+                    </div>
+                <?php endif ?>
+            </ul>
         </div>
     </div>
 </div>
