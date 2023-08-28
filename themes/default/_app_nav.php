d63aca24eedfb0b5d9c524ddec1ebd6d97c71dee
Lonnie Ezell <lonnieje@gmail.com>
2023-08-27 22:37:14 -0500

    Mobile nav links

--- themes/default/_app_nav.php
+++ themes/default/_app_nav.php
@@ -7,19 +7,22 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
                 <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <?php if(auth()->isLoggedIn()) : ?>
-                        <li>
-                            <a>Parents</a>
-                            <ul class="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
+                    <li>
+                        <a href="<?= site_url('discussions') ?>"
+                            <?= url_is('discussions') ? 'class="active"' : '' ?>>
+                            Discussions
+                        </a>
+                    </li>
+                    <?php if(!auth()->loggedIn()) : ?>
+                        <li><a href="<?= route_to('register') ?>">Sign Up</a></li>
+                        <li><a href="<?= route_to('login') ?>">Sign In</a></li>
                     <?php endif ?>
-                    <?php if(! auth()->isLoggedIn()) : ?>
-                        <li><a>Item 3</a></li>
+
+                    <?php if(auth()->loggedIn()) : ?>
+                        <li><a class="w-full">Account</a></li>
+                        <li><a class="w-full" href="<?= route_to('logout') ?>">Logout</a></li>
                     <?php endif ?>
+
                 </ul>
             </div>
 
