aba9d29a0ebf0a202cb6c38f6b46303e23c23e16
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2023-08-30 00:50:00 +0330

    style: add icon to menu

--- themes/default/_app_nav.php
+++ themes/default/_app_nav.php
@@ -19,8 +19,22 @@
                     <?php endif ?>
 
                     <?php if(auth()->loggedIn()) : ?>
-                        <li><a class="w-full">Account</a></li>
-                        <li><a class="w-full" href="<?= route_to('logout') ?>">Logout</a></li>
+                        <li>
+                            <a class="w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                Account
+                            </a>
+                        </li>
+                        <li>
+                            <a class="w-full" href="<?= route_to('logout') ?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                </svg>
+                                Logout
+                            </a>
+                        </li>
                     <?php endif ?>
 
                 </ul>
@@ -71,8 +85,22 @@
                             </div>
                         </label>
                         <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li><a class="w-full">Account</a></li>
-                            <li><a class="w-full" href="<?= route_to('logout') ?>">Logout</a></li>
+                            <li>
+                                <a class="w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    </svg>
+                                    Account
+                                </a>
+                            </li>
+                            <li>
+                                <a class="w-full" href="<?= route_to('logout') ?>">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                    </svg>
+                                    Logout
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 <?php endif ?>
