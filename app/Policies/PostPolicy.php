2f97c6c57d3a365460bbcc33943725a4b56f4c27
michalsn <michal@sniatala.pl>
2023-09-15 20:38:27 +0200

    cleanup policies

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -9,42 +9,10 @@ use App\Libraries\Policies\PolicyInterface;
 class PostPolicy implements PolicyInterface
 {
     /**
-     * Determines if the current user can create a new thread.
-     */
-    public function create(): bool
-    {
-        return true;
-    }
-
-    /**
      * Determines if the current user can edit a thread.
      */
     public function edit(User $user, Post $post): bool
     {
         return $user->can('posts.edit') || $user->id === $post->author_id;
-    }
-
-    /**
-     * Determines if the current user can delete a thread.
-     */
-    public function delete(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determines if the current user can reply to a thread.
-     */
-    public function reply(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determines if the current user can view a thread.
-     */
-    public function view(): bool
-    {
-        return true;
     }
 }
--- app/Policies/ThreadPolicy.php
+++ app/Policies/ThreadPolicy.php
@@ -9,42 +9,10 @@ use App\Libraries\Policies\PolicyInterface;
 class ThreadPolicy implements PolicyInterface
 {
     /**
-     * Determines if the current user can create a new thread.
-     */
-    public function create(): bool
-    {
-        return true;
-    }
-
-    /**
      * Determines if the current user can edit a thread.
      */
     public function edit(User $user, Thread $thread): bool
     {
         return $user->can('threads.edit') || $user->id === $thread->author_id;
-    }
-
-    /**
-     * Determines if the current user can delete a thread.
-     */
-    public function delete(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determines if the current user can reply to a thread.
-     */
-    public function reply(): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determines if the current user can view a thread.
-     */
-    public function view(): bool
-    {
-        return true;
     }
 }
