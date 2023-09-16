7114bde4915c70340360111511108fd04156751b
michalsn <michal@sniatala.pl>
2023-09-16 11:52:27 +0200

    fix issues with hx-target for replies

--- app/Views/discussions/posts/_post.php
+++ app/Views/discussions/posts/_post.php
@@ -48,7 +48,7 @@
             <?php if ($post->replies_count > 2): ?>
                 <div class="my-6 text-center">
                     <a class="btn btn-xs btn-primary"
-                       hx-get="<?= route_to('post-replies-all', $post->id); ?>"
+                       hx-get="<?= route_to('post-replies-load', $post->id); ?>"
                        hx-target="closest .post-replies"
                        hx-swap="innerHTML show:top"
                     >
@@ -60,8 +60,10 @@
                 <?= view('discussions/posts/_post', ['post' => $reply]) ?>
             <?php endforeach ?>
         </div>
+    <?php endif ?>
+    <?php if ($post->reply_to === null): ?>
         <div class="ml-10">
             <div id="post-reply-<?= $post->id; ?>" class="<?= $post->replies_count === 0 ? 'my-6' : '' ?>"></div>
         </div>
-    <?php endif ?>
+    <?php endif; ?>
 </div>
