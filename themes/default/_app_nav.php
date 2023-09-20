d617a3a1d906c92467070634b9c005df4f4fde08
michalsn <michal@sniatala.pl>
2023-09-20 19:32:34 +0200

    cleanup top nav

--- themes/default/_app_nav.php
+++ themes/default/_app_nav.php
@@ -13,12 +13,12 @@
                             Discussions
                         </a>
                     </li>
-                    <?php if(!auth()->loggedIn()) : ?>
-                        <li><a href="<?= route_to('register') ?>">Sign Up</a></li>
-                        <li><a href="<?= route_to('login') ?>">Sign In</a></li>
+                    <?php if (! auth()->loggedIn()): ?>
+                        <li><a href="<?= route_to('register') ?>" hx-boost="false">Sign Up</a></li>
+                        <li><a href="<?= route_to('login') ?>" hx-boost="false">Sign In</a></li>
                     <?php endif ?>
 
-                    <?php if(auth()->loggedIn()) : ?>
+                    <?php if (auth()->loggedIn()): ?>
                         <li>
                             <a class="w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -28,7 +28,7 @@
                             </a>
                         </li>
                         <li>
-                            <a class="w-full" href="<?= route_to('logout') ?>">
+                            <a class="w-full" href="<?= route_to('logout') ?>" hx-boost="false">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                                 </svg>
@@ -48,57 +48,62 @@
 
         <!-- Desktop Menu -->
         <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal px-1 gap-4 flex">
-                <li>
-                    <a href="<?= site_url('discussions') ?>"
-                        <?= url_is('discussions') ? 'class="active"' : '' ?>>
-                        Discussions
-                    </a>
-                </li>
-                <li>
-                    <a href="<?= site_url('members') ?>"
-                        <?= url_is('members') ? 'class="active"' : '' ?>>
-                        Members
-                    </a>
-                </li>
-
+            <div class="flex-none">
+                <ul class="menu menu-horizontal px-1 gap-4">
+                    <li>
+                        <a href="<?= site_url('discussions') ?>"
+                            <?= url_is('discussions') ? 'class="active"' : '' ?>>
+                            Discussions
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= site_url('members') ?>"
+                            <?= url_is('members') ? 'class="active"' : '' ?>>
+                            Members
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex-none px-4">
                 <div class="form-control">
                     <input type="text" placeholder="Search..." class="input input-bordered w-24 md:w-auto" />
                 </div>
-
-                <?php if(!auth()->loggedIn()) : ?>
-                    <li><a href="<?= route_to('register') ?>">Sign Up</a></li>
-                    <li><a href="<?= route_to('login') ?>">Sign In</a></li>
-                <?php endif ?>
-
-                <?php if(auth()->loggedIn()) : ?>
-                    <div class="dropdown dropdown-end">
-                        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                            <div class="w-10 rounded-full">
-                                <?= auth()->user()->renderAvatar() ?>
-                            </div>
-                        </label>
-                        <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a class="w-full">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    </svg>
-                                    Account
-                                </a>
-                            </li>
-                            <li>
-                                <a class="w-full" href="<?= route_to('logout') ?>">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                                    </svg>
-                                    Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                <?php endif ?>
-            </ul>
+            </div>
+            <div class="flex-none">
+                <ul class="menu menu-horizontal px-1 gap-4">
+                    <?php if (! auth()->loggedIn()): ?>
+                        <li><a href="<?= route_to('register') ?>" hx-boost="false">Sign Up</a></li>
+                        <li><a href="<?= route_to('login') ?>" hx-boost="false">Sign In</a></li>
+                    <?php endif ?>
+                </ul>
+            </div>
+            <?php if (auth()->loggedIn()): ?>
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                        <div class="w-10 rounded-full">
+                            <?= auth()->user()->renderAvatar() ?>
+                        </div>
+                    </label>
+                    <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a class="w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                Account
+                            </a>
+                        </li>
+                        <li>
+                            <a class="w-full" href="<?= route_to('logout') ?>" hx-boost="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                </svg>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            <?php endif ?>
         </div>
     </div>
 </div>
