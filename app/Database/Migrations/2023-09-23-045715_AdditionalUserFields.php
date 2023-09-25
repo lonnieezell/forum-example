73b7209f9458bf0c02b81759a0b2b916eef2fcb3
Michal Sniatala <michal@sniatala.pl>
2023-09-25 18:09:09 +0200

    cs-fix (#74)

--- app/Database/Migrations/2023-09-23-045715_AdditionalUserFields.php
+++ app/Database/Migrations/2023-09-23-045715_AdditionalUserFields.php
@@ -9,11 +9,11 @@ class AdditionalUserFields extends Migration
     public function up()
     {
         $this->forge->addColumn('users', [
-            'name' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'username',],
-            'company' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'timezone',],
-            'location' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'company',],
-            'website' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'location',],
-            'signature' => ['type' => 'TEXT', 'null' => true, 'after' => 'website',],
+            'name'      => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'username'],
+            'company'   => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'timezone'],
+            'location'  => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'company'],
+            'website'   => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true, 'after' => 'location'],
+            'signature' => ['type' => 'TEXT', 'null' => true, 'after' => 'website'],
         ]);
     }
 
