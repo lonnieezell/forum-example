3be21df465e4a18b45d5acd2d4b6574e4cd22827
Lonnie Ezell <lonnieje@gmail.com>
2023-10-04 23:01:56 -0500

    cs-fix

--- app/Cells/account_nav_cell.php
+++ app/Cells/account_nav_cell.php
@@ -1,12 +1,14 @@
-<div>
-<?php foreach ($pages as $page) : ?>
-    <li>
-        <a href="<?= $page['url'] ?>"
-            class="<?= url_is($page['url']) ? 'active' : '' ?>"
-        >
-            <?= $page['icon'] ?>
-            <span class="block text-centered sm:inline sm:text-left"><?= $page['title'] ?></span>
-        </a>
-    </li>
-<?php endforeach ?>
-</div>
+<?php if (isset($pages) && count($pages)) :?>
+    <div>
+    <?php foreach ($pages as $page) : ?>
+        <li>
+            <a href="<?= $page['url'] ?>"
+                class="<?= url_is($page['url']) ? 'active' : '' ?>"
+            >
+                <?= $page['icon'] ?>
+                <span class="block text-centered sm:inline sm:text-left"><?= $page['title'] ?></span>
+            </a>
+        </li>
+    <?php endforeach ?>
+    </div>
+<?php endif ?>
