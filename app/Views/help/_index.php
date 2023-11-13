2054e36c505eb297ac5db1aa4ad5c3043185b91e
michalsn <michal@sniatala.pl>
2023-11-13 20:08:24 +0100

    use slice()

--- app/Views/help/_index.php
+++ app/Views/help/_index.php
@@ -3,7 +3,7 @@
     <div class="inline-block" hx-boost="true">
         <ul class="menu bg-base-200 w-64 rounded-box">
             <li class="menu-title"><?= $category->getName(); ?></li>
-            <?php foreach ($category->getFiles()->take(5)->items() as $file): ?>
+            <?php foreach ($category->getFiles()->slice(0, 4)->items() as $file): ?>
                 <li><a href="<?= url_to('page', $file->urlPath()); ?>"><?= $file->getName(); ?></a></li>
             <?php endforeach; ?>
         </ul>
