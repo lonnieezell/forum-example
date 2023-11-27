b92c1a70d832b4cf1c681d5a43ec14f85464d5e6
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2023-11-27 09:32:27 +0330

    shield b7 to b8

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of CodeIgniter Shield.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Config;
 
 use App\Libraries\Authentication\Actions\TwoFactorAuthEmail;
@@ -28,9 +37,9 @@ class Auth extends ShieldAuth
      */
 
     // Constants for Record Login Attempts. Do not change.
-    final public const RECORD_LOGIN_ATTEMPT_NONE    = 0; // Do not record at all
-    final public const RECORD_LOGIN_ATTEMPT_FAILURE = 1; // Record only failures
-    final public const RECORD_LOGIN_ATTEMPT_ALL     = 2; // Record all login attempts
+    public const RECORD_LOGIN_ATTEMPT_NONE    = 0; // Do not record at all
+    public const RECORD_LOGIN_ATTEMPT_FAILURE = 1; // Record only failures
+    public const RECORD_LOGIN_ATTEMPT_ALL     = 2; // Record all login attempts
 
     /**
      * --------------------------------------------------------------------
@@ -66,10 +75,12 @@ class Auth extends ShieldAuth
      * to apply any logic you may need.
      */
     public array $redirects = [
-        'register'    => '/',
-        'login'       => '/',
-        'logout'      => 'login',
-        'force_reset' => '/',
+        'register'          => '/',
+        'login'             => '/',
+        'logout'            => 'login',
+        'force_reset'       => '/',
+        'permission_denied' => '/',
+        'group_denied'      => '/',
     ];
 
     /**
@@ -153,7 +164,7 @@ class Auth extends ShieldAuth
      * logged-in user on every page request.
      * This feature only works when session/tokens filter is active.
      *
-     * @see https://codeigniter4.github.io/shield/install/#protect-all-pages for set filters.
+     * @see https://codeigniter4.github.io/shield/quick_start_guide/using_session_auth/#protecting-pages for set filters.
      */
     public bool $recordActiveDate = true;
 
@@ -355,14 +366,14 @@ class Auth extends ShieldAuth
      * --------------------------------------------------------------------
      * The BCRYPT method of hashing allows you to define the "cost"
      * or number of iterations made, whenever a password hash is created.
-     * This defaults to a value of 10 which is an acceptable number.
+     * This defaults to a value of 12 which is an acceptable number.
      * However, depending on the security needs of your application
      * and the power of your hardware, you might want to increase the
      * cost. This makes the hashing process takes longer.
      *
      * Valid range is between 4 - 31.
      */
-    public int $hashCost = 10;
+    public int $hashCost = 12;
 
     /**
      * If you need to support passwords saved in versions prior to Shield v1.0.0-beta.4.
@@ -477,6 +488,28 @@ class Auth extends ShieldAuth
     }
 
     /**
+     * Returns the URL the user should be redirected to
+     * if permission denied.
+     */
+    public function permissionDeniedRedirect(): string
+    {
+        $url = setting('Auth.redirects')['permission_denied'];
+
+        return $this->getUrl($url);
+    }
+
+    /**
+     * Returns the URL the user should be redirected to
+     * if group denied.
+     */
+    public function groupDeniedRedirect(): string
+    {
+        $url = setting('Auth.redirects')['group_denied'];
+
+        return $this->getUrl($url);
+    }
+
+    /**
      * Accepts a string which can be an absolute URL or
      * a named route or just a URI path, and returns the
      * full path.
@@ -485,10 +518,23 @@ class Auth extends ShieldAuth
      */
     protected function getUrl(string $url): string
     {
-        return match (true) {
-            str_starts_with($url, 'http://') || str_starts_with($url, 'https://') => $url,
-            route_to($url) !== false                                              => rtrim(url_to($url), '/ '),
-            default                                                               => rtrim(site_url($url), '/ '),
-        };
+        // To accommodate all url patterns
+        $final_url = '';
+
+        switch (true) {
+            case strpos($url, 'http://') === 0 || strpos($url, 'https://') === 0: // URL begins with 'http' or 'https'. E.g. http://example.com
+                $final_url = $url;
+                break;
+
+            case route_to($url) !== false: // URL is a named-route
+                $final_url = rtrim(url_to($url), '/ ');
+                break;
+
+            default: // URL is a route (URI path)
+                $final_url = rtrim(site_url($url), '/ ');
+                break;
+        }
+
+        return $final_url;
     }
 }
