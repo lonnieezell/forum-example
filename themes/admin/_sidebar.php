5fb6d4366f5b5ff1711aaa0d388db6e936d8cef1
Lonnie Ezell <lonnieje@gmail.com>
2024-01-08 23:26:26 -0600

    feat: Basic admin panel and user settings page.

--- themes/admin/_sidebar.php
+++ themes/admin/_sidebar.php
@@ -1,1 +1,16 @@
-Admin Sidebar
+<nav class="side-nav">
+    <!-- Settings -->
+    <div x-data="{ open: true }">
+        <p class="side-nav-title" x-on:click="open = ! open">
+            Settings
+            <span>&#709;</span>
+        </p>
+        <menu x-show="open" x-transition>
+            <li>
+                <a href="<?= url_to('settings-users') ?>">
+                    Users
+                </a>
+            </li>
+        </menu>
+    </div>
+</nav>
