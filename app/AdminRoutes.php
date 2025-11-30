a189c4eebb20b774da3fc4250c516c63352e2430
neznaika0 <ozornick.ks@gmail.com>
2025-12-01 00:14:17 +0300

    fix: Uppercase for HTTP methods (#396)

--- app/AdminRoutes.php
+++ app/AdminRoutes.php
@@ -1,5 +1,6 @@
 <?php
 
+use CodeIgniter\HTTP\Method;
 use CodeIgniter\Router\RouteCollection;
 
 /**
@@ -10,7 +11,7 @@ $routes->group('admin', static function (RouteCollection $routes) {
 
     // Settings
     $routes->group('settings', static function (RouteCollection $routes) {
-        $routes->match(['get', 'post'], 'users', 'Admin\Settings\UsersController::index', ['as' => 'settings-users']);
-        $routes->match(['get', 'post'], 'trust-levels', 'Admin\Settings\TrustLevelsController::index', ['as' => 'settings-trust']);
+        $routes->match([Method::GET, Method::POST], 'users', 'Admin\Settings\UsersController::index', ['as' => 'settings-users']);
+        $routes->match([Method::GET, Method::POST], 'trust-levels', 'Admin\Settings\TrustLevelsController::index', ['as' => 'settings-trust']);
     });
 });
