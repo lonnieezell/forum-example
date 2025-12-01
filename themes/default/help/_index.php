d0e7d849950927a7d1e9440e4738d96e15fe8163
neznaika0 <ozornick.ks@gmail.com>
2025-12-01 14:06:56 +0300

    fix: Improved design for Help section

--- themes/default/help/_index.php
+++ themes/default/help/_index.php
@@ -1,7 +1,9 @@
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+
 <?php foreach ($pages->dirs(null, [1])->items() as $category): ?>
 
     <div class="inline-block" hx-boost="true">
-        <ul class="menu bg-base-200 w-64 rounded-box">
+        <ul class="menu bg-base-200 rounded-box">
             <li class="menu-title"><?= $category->getName(); ?></li>
             <?php foreach ($category->getFiles()->slice(0, 4)->items() as $file): ?>
                 <li><a href="<?= url_to('page', $file->getPath()); ?>"><?= $file->getName(); ?></a></li>
@@ -10,3 +12,5 @@
     </div>
 
 <?php endforeach; ?>
+
+</div>
